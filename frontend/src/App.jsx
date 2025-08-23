ab468fd8962bde806b2972bd83d740dcb1ab7b84
Nafi Bin Zafar <nafi.zafar179@gmail.com>
2025-08-23 20:00:38 +0600

    RootLayout implementation and all routes in routes.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 // src/App.jsx
 
-import React from 'react';
-import AppRoute from './services/appRoute.jsx';
+import React from "react";
+import AppRoute from "./services/appRoute.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.jsx";
 
 function App() {
-  return (
-    <div>
-      <AppRoute />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
